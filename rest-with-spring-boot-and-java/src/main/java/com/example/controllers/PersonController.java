6fc673c1b24d0308ffaf8920f8d1af5030fffef5
Anderson Rodrigues <and.rt@hotmail.com>
2024-03-08 16:14:06 -0300

    Seção 13: Content Negotiation

--- rest-with-spring-boot-and-java/src/main/java/com/example/controllers/PersonController.java
+++ rest-with-spring-boot-and-java/src/main/java/com/example/controllers/PersonController.java
@@ -2,8 +2,8 @@ package com.example.controllers;
 
 import com.example.data.vo.v1.PersonVO;
 import com.example.services.PersonServices;
+import com.example.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,24 +16,24 @@ public class PersonController {
     private PersonServices service;
 
     @GetMapping(value = "/{id}",
-                produces = MediaType.APPLICATION_JSON_VALUE)
+                produces = {com.example.util.MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception{
         return service.findById(id);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public List<PersonVO> findAll() throws Exception{
         return service.findAll();
     }
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-                produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+                produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public PersonVO create(@RequestBody PersonVO person){
         return service.create(person);
     }
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
-                produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+                produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public PersonVO update(@RequestBody PersonVO person){
         return service.update(person);
     }
--- rest-with-spring-boot-and-java/src/main/java/com/example/data/vo/v1/PersonVO.java
+++ rest-with-spring-boot-and-java/src/main/java/com/example/data/vo/v1/PersonVO.java
@@ -7,17 +7,14 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import java.io.Serial;
 import java.io.Serializable;
 
-@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
 public class PersonVO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
     private Long id;
 
-    @JsonProperty("first_name")
     private String firstName;
 
-    @JsonProperty("last_name")
     private String lastName;
 
     private String address;
