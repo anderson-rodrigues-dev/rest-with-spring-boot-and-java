d7eddf868e4855d8a77ae47410d157d386f55ff6
Anderson Rodrigues <and.rt@hotmail.com>
2024-03-24 16:24:38 -0300

    Seção 18: Blindando nossa API com Rest Assured, Testcontainers e JUnit 5

---
+++ rest-with-spring-boot-and-java/src/test/java/com/example/integrationtests/controller/withxml/BookControllerXmlTest.java
@@ -0,0 +1,252 @@
+package com.example.integrationtests.controller.withxml;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Date;
+import java.util.List;
+
+import com.example.configs.TestConfigs;
+import com.example.data.vo.v1.BookVO;
+import com.example.integrationtests.testcontainers.AbstractIntegrationTest;
+import com.example.integrationtests.vo.AccountCredentialsVO;
+import com.example.integrationtests.vo.TokenVO;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(OrderAnnotation.class)
+public class BookControllerXmlTest extends AbstractIntegrationTest {
+	
+	private static RequestSpecification specification;
+	private static ObjectMapper objectMapper;
+
+	private static BookVO book;
+	
+	@BeforeAll
+	public static void setup() {
+		objectMapper = new ObjectMapper();
+		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		
+		book = new BookVO();
+	}
+	
+	@Test
+	@Order(0)
+	public void authorization() {
+		
+		AccountCredentialsVO user = new AccountCredentialsVO("leandro", "admin123");
+		
+		var accessToken = given()
+				.basePath("/auth/signin")
+					.port(TestConfigs.SERVER_PORT)
+					.contentType(TestConfigs.CONTENT_TYPE_XML)
+					.accept(TestConfigs.CONTENT_TYPE_XML)
+				.body(user)
+					.when()
+				.post()
+					.then()
+						.statusCode(200)
+							.extract()
+							.body()
+								.as(TokenVO.class)
+							.getAccessToken();
+		
+		specification = new RequestSpecBuilder()
+				.addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + accessToken)
+				.setBasePath("/api/book/v1")
+				.setPort(TestConfigs.SERVER_PORT)
+					.addFilter(new RequestLoggingFilter(LogDetail.ALL))
+					.addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+				.build();
+	}
+	
+	@Test
+	@Order(1)
+	public void testCreate() throws JsonProcessingException {
+		mockBook();
+		
+		var content = given().spec(specification)
+				.contentType(TestConfigs.CONTENT_TYPE_XML)
+				.accept(TestConfigs.CONTENT_TYPE_XML)
+					.body(book)
+					.when()
+					.post()
+				.then()
+					.statusCode(200)
+						.extract()
+						.body()
+							.asString();
+		
+        book = objectMapper.readValue(content, BookVO.class);
+        
+        assertNotNull(book.getKey());
+        assertNotNull(book.getTitle());
+        assertNotNull(book.getAuthor());
+        assertNotNull(book.getPrice());
+        assertTrue(book.getKey() > 0);
+        assertEquals("Docker Deep Dive", book.getTitle());
+        assertEquals("Nigel Poulton", book.getAuthor());
+        assertEquals(55.99, book.getPrice());
+	}
+
+	@Test
+	@Order(2)
+	public void testUpdate() throws JsonProcessingException {
+
+        book.setTitle("Docker Deep Dive - Updated");
+		
+		var content = given().spec(specification)
+				.contentType(TestConfigs.CONTENT_TYPE_XML)
+				.accept(TestConfigs.CONTENT_TYPE_XML)
+					.body(book)
+					.when()
+					.post()
+				.then()
+					.statusCode(200)
+						.extract()
+						.body()
+							.asString();
+		
+        BookVO bookUpdated = objectMapper.readValue(content, BookVO.class);
+        
+        assertNotNull(bookUpdated.getKey());
+        assertNotNull(bookUpdated.getTitle());
+        assertNotNull(bookUpdated.getAuthor());
+        assertNotNull(bookUpdated.getPrice());
+        assertEquals(bookUpdated.getKey(), book.getKey());
+        assertEquals("Docker Deep Dive - Updated", bookUpdated.getTitle());
+        assertEquals("Nigel Poulton", bookUpdated.getAuthor());
+        assertEquals(55.99, bookUpdated.getPrice());
+	}
+
+	@Test
+	@Order(3)
+	public void testFindById() throws JsonProcessingException {
+		mockBook();
+			
+		var content = given().spec(specification)
+				.contentType(TestConfigs.CONTENT_TYPE_XML)
+				.accept(TestConfigs.CONTENT_TYPE_XML)
+					.pathParam("id", book.getKey())
+					.when()
+					.get("{id}")
+				.then()
+					.statusCode(200)
+						.extract()
+						.body()
+							.asString();
+		
+		BookVO foundBook = objectMapper.readValue(content, BookVO.class);
+        
+        assertNotNull(foundBook.getKey());
+        assertNotNull(foundBook.getTitle());
+        assertNotNull(foundBook.getAuthor());
+        assertNotNull(foundBook.getPrice());
+        assertEquals(foundBook.getKey(), book.getKey());
+        assertEquals("Docker Deep Dive - Updated", foundBook.getTitle());
+        assertEquals("Nigel Poulton", foundBook.getAuthor());
+        assertEquals(55.99, foundBook.getPrice());
+	}
+	
+	@Test
+	@Order(4)
+	public void testDelete() {
+
+		given().spec(specification)
+			.contentType(TestConfigs.CONTENT_TYPE_XML)
+			.accept(TestConfigs.CONTENT_TYPE_XML)
+				.pathParam("id", book.getKey())
+				.when()
+				.delete("{id}")
+			.then()
+				.statusCode(204);
+	}
+	
+	@Test
+	@Order(5)
+	public void testFindAll() throws JsonProcessingException {
+		
+		var content = given().spec(specification)
+				.contentType(TestConfigs.CONTENT_TYPE_XML)
+				.accept(TestConfigs.CONTENT_TYPE_XML)
+					.when()
+					.get()
+				.then()
+					.statusCode(200)
+						.extract()
+						.body()
+							.asString();
+		
+		List<BookVO> books = objectMapper.readValue(content, new TypeReference<>() {
+        });
+		
+		BookVO foundBookOne = books.getFirst();
+        
+        assertNotNull(foundBookOne.getKey());
+        assertNotNull(foundBookOne.getTitle());
+        assertNotNull(foundBookOne.getAuthor());
+        assertNotNull(foundBookOne.getPrice());
+        assertTrue(foundBookOne.getKey() > 0);
+        assertEquals("Working effectively with legacy code", foundBookOne.getTitle());
+        assertEquals("Michael C. Feathers", foundBookOne.getAuthor());
+        assertEquals(49.00, foundBookOne.getPrice());
+        
+        BookVO foundBookFive = books.getFirst();
+        
+        assertNotNull(foundBookFive.getKey());
+        assertNotNull(foundBookFive.getTitle());
+        assertNotNull(foundBookFive.getAuthor());
+        assertNotNull(foundBookFive.getPrice());
+        assertTrue(foundBookFive.getKey() > 0);
+        assertEquals("Code complete", foundBookFive.getTitle());
+        assertEquals("Steve McConnell", foundBookFive.getAuthor());
+        assertEquals(58.0, foundBookFive.getPrice());
+	}
+
+	
+	@Test
+	@Order(6)
+	public void testFindAllWithoutToken() {
+		
+		RequestSpecification specificationWithoutToken = new RequestSpecBuilder()
+			.setBasePath("/api/book/v1")
+			.setPort(TestConfigs.SERVER_PORT)
+				.addFilter(new RequestLoggingFilter(LogDetail.ALL))
+				.addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+			.build();
+		
+		given().spec(specificationWithoutToken)
+			.contentType(TestConfigs.CONTENT_TYPE_XML)
+			.accept(TestConfigs.CONTENT_TYPE_XML)
+				.when()
+				.get()
+			.then()
+				.statusCode(403);
+	}
+	
+    private void mockBook() {
+        book.setTitle("Docker Deep Dive");
+        book.setAuthor("Nigel Poulton");
+        book.setPrice(55.99);
+        book.setLaunchDate(new Date());
+    }  
+}
---
+++ rest-with-spring-boot-and-java/src/test/java/com/example/integrationtests/controller/withyml/BookControllerYamlTest.java
@@ -0,0 +1,258 @@
+package com.example.integrationtests.controller.withyml;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import com.example.configs.TestConfigs;
+import com.example.data.vo.v1.BookVO;
+import com.example.integrationtests.controller.withyml.mapper.YamlMapper;
+import com.example.integrationtests.testcontainers.AbstractIntegrationTest;
+import com.example.integrationtests.vo.AccountCredentialsVO;
+import com.example.integrationtests.vo.TokenVO;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.EncoderConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(OrderAnnotation.class)
+public class BookControllerYamlTest extends AbstractIntegrationTest {
+
+    private static RequestSpecification specification;
+
+    private static YamlMapper objectMapper;
+
+    private static BookVO book;
+
+    @BeforeAll
+    public static void setup() {
+        objectMapper = new YamlMapper();
+        book = new BookVO();
+    }
+    
+    @Test
+    @Order(1)
+    public void authorization() {
+        AccountCredentialsVO user = new AccountCredentialsVO();
+        user.setUsername("leandro");
+        user.setPassword("admin123");
+
+        var token =
+                given()
+                    .config(
+                        RestAssuredConfig
+                            .config()
+                            .encoderConfig(EncoderConfig.encoderConfig()
+                                    .encodeContentTypeAs(TestConfigs.CONTENT_TYPE_YAML, ContentType.TEXT)))
+                    .basePath("/auth/signin")
+                    .port(TestConfigs.SERVER_PORT)
+                    .contentType(TestConfigs.CONTENT_TYPE_YAML)
+    				.accept(TestConfigs.CONTENT_TYPE_YAML)
+                    .body(user, objectMapper)
+                    .when()
+                        .post()
+                    .then()
+                        .statusCode(200)
+                    .extract()
+                    .body()
+                        .as(TokenVO.class, objectMapper)
+                    .getAccessToken();
+
+            specification =
+                new RequestSpecBuilder()
+                    .addHeader(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + token)
+                    .setBasePath("/api/book/v1")
+                    .setPort(TestConfigs.SERVER_PORT)
+                    .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                    .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                    .build();
+    }
+      
+    @Test
+    @Order(2)
+    public void testCreate() {
+        
+        mockBook();
+
+        book = given()
+                    .config(
+                        RestAssuredConfig
+                            .config()
+                            .encoderConfig(EncoderConfig.encoderConfig()
+                                    .encodeContentTypeAs(TestConfigs.CONTENT_TYPE_YAML, ContentType.TEXT)))
+                    .spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_YAML)
+				.accept(TestConfigs.CONTENT_TYPE_YAML)
+                    .body(book, objectMapper)
+                    .when()
+                    .post()
+                .then()
+                    .statusCode(200)
+                        .extract()
+                        .body()
+                            .as(BookVO.class, objectMapper);
+        
+        assertNotNull(book.getKey());
+        assertNotNull(book.getTitle());
+        assertNotNull(book.getAuthor());
+        assertNotNull(book.getPrice());
+        assertTrue(book.getKey() > 0);
+        assertEquals("Docker Deep Dive", book.getTitle());
+        assertEquals("Nigel Poulton", book.getAuthor());
+        assertEquals(55.99, book.getPrice());
+    }
+      
+    @Test
+    @Order(3)
+    public void testUpdate() {
+        
+        book.setTitle("Docker Deep Dive - Updated");
+
+        BookVO bookUpdated = given()
+                    .config(
+                        RestAssuredConfig
+                            .config()
+                            .encoderConfig(EncoderConfig.encoderConfig()
+                                    .encodeContentTypeAs(TestConfigs.CONTENT_TYPE_YAML, ContentType.TEXT)))
+                    .spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_YAML)
+				.accept(TestConfigs.CONTENT_TYPE_YAML)
+                    .body(book, objectMapper)
+                    .when()
+                    .put()
+                .then()
+                    .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(BookVO.class, objectMapper);
+        
+        assertNotNull(bookUpdated.getKey());
+        assertNotNull(bookUpdated.getTitle());
+        assertNotNull(bookUpdated.getAuthor());
+        assertNotNull(bookUpdated.getPrice());
+        assertEquals(bookUpdated.getKey(), book.getKey());
+        assertEquals("Docker Deep Dive - Updated", bookUpdated.getTitle());
+        assertEquals("Nigel Poulton", bookUpdated.getAuthor());
+        assertEquals(55.99, bookUpdated.getPrice());
+    }
+
+    @Test
+    @Order(4)
+    public void testFindById() {
+        var foundBook = given()
+                    .config(
+                        RestAssuredConfig
+                            .config()
+                            .encoderConfig(EncoderConfig.encoderConfig()
+                                    .encodeContentTypeAs(TestConfigs.CONTENT_TYPE_YAML, ContentType.TEXT)))
+                    .spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_YAML)
+				.accept(TestConfigs.CONTENT_TYPE_YAML)
+                    .pathParam("id", book.getKey())
+                    .when()
+                    .get("{id}")
+                .then()
+                    .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(BookVO.class, objectMapper);
+        
+        assertNotNull(foundBook.getKey());
+        assertNotNull(foundBook.getTitle());
+        assertNotNull(foundBook.getAuthor());
+        assertNotNull(foundBook.getPrice());
+        assertEquals(foundBook.getKey(), book.getKey());
+        assertEquals("Docker Deep Dive - Updated", foundBook.getTitle());
+        assertEquals("Nigel Poulton", foundBook.getAuthor());
+        assertEquals(55.99, foundBook.getPrice());
+    }
+    
+    @Test
+    @Order(5)
+    public void testDelete() {
+        given()
+            .config(
+                RestAssuredConfig
+                    .config()
+                    .encoderConfig(EncoderConfig.encoderConfig()
+                            .encodeContentTypeAs(TestConfigs.CONTENT_TYPE_YAML, ContentType.TEXT)))
+            .spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_YAML)
+				.accept(TestConfigs.CONTENT_TYPE_YAML)
+                    .pathParam("id", book.getKey())
+                    .when()
+                    .delete("{id}")
+                .then()
+                    .statusCode(204);
+    }
+    
+    @Test
+    @Order(6)
+    public void testFindAll() {
+        var response = given()
+                    .config(
+                        RestAssuredConfig
+                            .config()
+                            .encoderConfig(EncoderConfig.encoderConfig()
+                                    .encodeContentTypeAs(TestConfigs.CONTENT_TYPE_YAML, ContentType.TEXT)))
+                    .spec(specification)
+                .contentType(TestConfigs.CONTENT_TYPE_YAML)
+				.accept(TestConfigs.CONTENT_TYPE_YAML)
+                    .when()
+                    .get()
+                .then()
+                    .statusCode(200)
+                        .extract()
+                        .body()
+                        .as(BookVO[].class, objectMapper); 
+
+
+        List<BookVO> content = Arrays.asList(response);
+
+        BookVO foundBookOne = content.getFirst();
+        
+        assertNotNull(foundBookOne.getKey());
+        assertNotNull(foundBookOne.getTitle());
+        assertNotNull(foundBookOne.getAuthor());
+        assertNotNull(foundBookOne.getPrice());
+        assertTrue(foundBookOne.getKey() > 0);
+        assertEquals("Working effectively with legacy code", foundBookOne.getTitle());
+        assertEquals("Michael C. Feathers", foundBookOne.getAuthor());
+        assertEquals(49.00, foundBookOne.getPrice());
+        
+        BookVO foundBookFive = content.get(4);
+        
+        assertNotNull(foundBookFive.getKey());
+        assertNotNull(foundBookFive.getTitle());
+        assertNotNull(foundBookFive.getAuthor());
+        assertNotNull(foundBookFive.getPrice());
+        assertTrue(foundBookFive.getKey() > 0);
+        assertEquals("Code complete", foundBookFive.getTitle());
+        assertEquals("Steve McConnell", foundBookFive.getAuthor());
+        assertEquals(58.0, foundBookFive.getPrice());
+    }
+     
+    private void mockBook() {
+        book.setTitle("Docker Deep Dive");
+        book.setAuthor("Nigel Poulton");
+        book.setPrice(55.99);
+        book.setLaunchDate(new Date());
+    }
+}
