f06454e61707783710d8e811bcaecd01c93ec43d
Anderson Rodrigues <and.rt@hotmail.com>
2024-05-05 20:51:48 -0300

    Continuous Integration with GitHub, GitHub Actions, Amazon ECR ans ECS

--- rest-with-spring-boot-and-java/src/main/java/com/example/config/OpenAPIConfig.java
+++ rest-with-spring-boot-and-java/src/main/java/com/example/config/OpenAPIConfig.java
@@ -12,7 +12,7 @@ public class OpenAPIConfig {
     public OpenAPI customOpenAPI(){
         return new OpenAPI()
                 .info(new Info()
-                        .title("RESTful API with Java 18 and Spring Boot 3")
+                        .title("RESTful API with Java 18 and Spring Boot 3 - Foo Bar")
                         .version("v1")
                         .description("Some description about your API")
                         .termsOfService("https://github.com/anderson-rodrigues-dev")
